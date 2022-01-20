1724837bedcca4a676867bb42b4b30c80299ba01
DESKTOP-3J65I2E\admin <topdirector2017@gmail.com>
2022-01-21 08:23:13 +0900

    add standard event Transfer

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,7 +1,8 @@
-import {LogSponsorLinkedEntity} from "../types";
+import {LogSponsorLinkedEntity, TransferEntity} from "../types";
 import { MoonbeamEvent } from '@subql/contract-processors/dist/moonbeam';
 import { BigNumber } from "ethers";
 
+type TransferEventArgs = [string, string, BigNumber] & { from: string; to: string; tokenId: BigNumber; };
 type LogSponsorLinkedEventArgs = [string, string] & { sponsor: string; affiliate: string; };
 
 export async function handleLogSponsorLinkedEvent(event: MoonbeamEvent<LogSponsorLinkedEventArgs>): Promise<void> {
@@ -16,4 +17,20 @@ export async function handleLogSponsorLinkedEvent(event: MoonbeamEvent<LogSponso
 
 	await logSponsorLinked_value.save();
     logger.warn("Chai log info handleLogSponsorLinkedEvent end!");
+}
+
+export async function handleTransferEvent(event: MoonbeamEvent<TransferEventArgs>): Promise<void> {
+    logger.warn("Chai log info handleTransferEvent start!");
+	const transferEntity_value = new TransferEntity(event.transactionHash);
+
+    logger.warn("Chai log info handleTransferEvent start1!");
+    transferEntity_value.from = event.args.from;
+    logger.warn("Chai log info handleTransferEvent start2!");
+    transferEntity_value.to = event.args.to;
+    logger.warn("Chai log info handleTransferEvent start3!");
+    transferEntity_value.tokenId = event.args.tokenId.toBigInt();
+    logger.warn("Chai log info handleTransferEvent start4!");
+
+	await transferEntity_value.save();
+    logger.warn("Chai log info handleTransferEvent end!");
 }
